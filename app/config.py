4834b87cbad2441bd32fda38ad6808e36dc21adf
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 11:15:54 +0300

    [CHORE #160363979]-Configurations setup

---
+++ app/config.py
@@ -0,0 +1,30 @@
+class BaseConfig(object):
+    """Common configurations"""
+    TESTING = False
+    DEBUG = False
+    SECRET_KEY = "qwertyuiop"
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configurations"""
+    DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/stackoverflow'
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    """Configurations for Testing, with a separate test database."""
+    DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/stackoverflow_tests'
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configurations"""
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
