8997718bef062c35b35de14401b0fdd394611ae7
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 12:56:17 +0300

    [CHORE #160364196]- Questions model class created

--- app/models.py
+++ app/models.py
@@ -3,3 +3,24 @@ class User:
         self.username = username
         self.email = email
         self.password = password
+
+
+class Question:
+    def __init__(self, title, question, qstn_owner, date):
+        self.title = title
+        self.question = question
+        self.qstn_owner = qstn_owner
+        self.date = date
+
+    def to_json(self):
+        question = dict(
+            title=self.title,
+            question=self.question,
+            qstn_owner=self.qstn_owner,
+            date=self.date
+        )
+        return question
+
+    # method to display class objects as dictionaries
+    def __repr__(self):
+        return repr(self.__dict__)
