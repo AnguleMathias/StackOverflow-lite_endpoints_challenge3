d5b979462b8a94c164952b47a7552d554ddc463d
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 17:36:22 +0300

    [FEATURE #160255557]- Initializing JWTManager with flask application

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 from app.auth.authentication import auth_blueprint
 
 app.register_blueprint(auth_blueprint)
+
+app.config['JWT_SECRET_KEY'] = 'qwertyuiop'
+from flask_jwt_extended import JWTManager
+jwt = JWTManager(app)
