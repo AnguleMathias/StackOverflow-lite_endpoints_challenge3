8111534c1eca14ee320c0e6d2de6d879a4aa85fb
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 11:17:19 +0300

    [CHORE #160363979]-Configuration import optimized

--- app/db/dbManager.py
+++ app/db/dbManager.py
@@ -1,18 +1,19 @@
 import psycopg2
 import psycopg2.extras as extra
+from app.config import app_config
 
 
 class DBConnection:
     def __init__(self):
         if app_config["development"]:
-            self.con = psycopg2.connect(database="stackoverflow", user="postgres", password="mathias", host="localhost",
-                                        port="5432")
+            self.con = psycopg2.connect(database="stackoverflow", user="postgres", password="mathias",
+                                        host="localhost", port="5432")
             self.con.autocommit = True
             self.cursor = self.con.cursor()
             self.dict_cursor = self.con.cursor(cursor_factory=extra.RealDictCursor)
         elif app_config["testing"]:
             self.con = psycopg2.connect(database="stackoverflow_tests", user="postgres", password="mathias",
-                                       host="localhost", port="5432")
+                                        host="localhost", port="5432")
             self.con.autocommit = True
             self.cursor = self.con.cursor()
             self.dict_cursor = self.con.cursor(cursor_factory=extra.RealDictCursor)
