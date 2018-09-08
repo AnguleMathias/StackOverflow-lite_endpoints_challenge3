ea5399c3d7374b70666b5b2db65bfa350074ba46
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 11:24:34 +0300

    [CHORE #160255224]-Create table answers

--- app/db/dbManager.py
+++ app/db/dbManager.py
@@ -38,6 +38,17 @@ class DBConnection:
                 qstn_owner VARCHAR(20) NOT NULL,
                 date timestamp NOT NULL
             )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS answers (
+                ans_id SERIAL PRIMARY KEY,
+                answer VARCHAR(250) NOT NULL,
+                ans_owner VARCHAR(20) NOT NULL,
+                qstn_id INTEGER NOT NULL,
+                votes INTEGER NOT NULL,
+                status VARCHAR(250) NOT NULL,
+                date timestamp NOT NULL
+            )
             """
         )
         for query in queries:
