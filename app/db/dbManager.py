5dbd419b8c3068cff04b181a2bfd3059a0880ed2
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 11:20:10 +0300

    [CHORE #160255224]-Create table users

--- app/db/dbManager.py
+++ app/db/dbManager.py
@@ -17,3 +17,18 @@ class DBConnection:
             self.con.autocommit = True
             self.cursor = self.con.cursor()
             self.dict_cursor = self.con.cursor(cursor_factory=extra.RealDictCursor)
+
+    def create_tables(self):
+
+        queries = (
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                user_id SERIAL PRIMARY KEY,
+                username VARCHAR(50) NOT NULL,
+                email VARCHAR(50) NOT NULL,
+                password VARCHAR(25) NOT NULL
+            )
+            """
+        )
+        for query in queries:
+            self.cursor.execute(query)
