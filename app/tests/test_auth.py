dd02ec6879032cf349cc22b6d7c2b1b76a57b5a1
Angule Mathias <angulemathias3@gmail.com>
2018-09-08 14:38:36 +0300

    [CHORE #160364432]- Test successful registration update

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,14 +1,14 @@
 from .base_test import BaseTestCase
-from flask import jsonify, json
+from flask import json
 
 
 class TestAuth(BaseTestCase):
 
     def test_registration(self):
         """ Test for successful user register """
-        response = self.app.post("/api/auth/register",
+        response = self.app.post("/api/v1/auth/register",
                                  content_type='application/json',
                                  data=json.dumps(
                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
                                  )
-        self.assertEquals(response.status_code, 201)
+        self.assertEqual(response.status_code, 201)
