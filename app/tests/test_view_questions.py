f99fa6e91a0abd90a4379c52b550786904a3f83c
Angule Mathias <angulemathias3@gmail.com>
2018-09-09 10:19:54 +0300

    [CHORE #160364432]- Test view question with db

--- app/tests/test_view_questions.py
+++ app/tests/test_view_questions.py
@@ -4,5 +4,21 @@ from flask import json
 
 class TestViewQuestion(BaseTestCase):
 
-    def test_viewing_all_questions(self):
-        pass
+    def test_viewing_questions_with_db(self):
+        """ Test viewing questions """
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@email.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response3 = self.app.get("/api/v1/questions", content_type='application/json',
+                                 headers=dict(Authorization='Bearer ' + reply2[1]['token']))
+        reply = json.loads(response3.data)
+        self.assertEquals(reply.get("message"), "No questions posted yet")
+        self.assertEquals(response3.status_code, 404)
