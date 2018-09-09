192e7f0da782294a9a35829a5f7c7a3e8e076245
Angule Mathias <angulemathias3@gmail.com>
2018-09-10 00:53:22 +0300

    [CHORE #160364432]- Initial test post answer

--- app/tests/test_post_answer.py
+++ app/tests/test_post_answer.py
@@ -1,2 +1,30 @@
 from base_test import BaseTestCase
 from flask import json
+
+
+class TestPostAnswer(BaseTestCase):
+
+    def test_posting_answer(self):
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(
+                                      dict(title="What", question="What is your question?"), )
+                                  )
+        response3 = self.app.post("/api/v1/questions/1/answers",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(dict(answer="What is your question?"), )
+                                  )
+        self.assertEquals(response3.status_code, 201)
