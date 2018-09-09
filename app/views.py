7fd5b8fc9c8e0c67cbaecb297e303cb76b81e2ed
Angule Mathias <angulemathias3@gmail.com>
2018-09-09 09:49:05 +0300

    [#160256391]- GET all question Blueprint url

--- app/views.py
+++ app/views.py
@@ -61,7 +61,8 @@ class FetchAllQuestions(MethodView):
         return jsonify({"message": "No questions posted yet"}), 404
 
 
-
 post_question_view = PostQuestion.as_view("post_question_view")
+fetch_questions_view = FetchAllQuestions.as_view("fetch_questions_view")
 
 question_blueprint.add_url_rule("/api/v1/questions", view_func=post_question_view, methods=["POST"])
+question_blueprint.add_url_rule("/api/v1/questions", view_func=fetch_questions_view, methods=["GET"])
