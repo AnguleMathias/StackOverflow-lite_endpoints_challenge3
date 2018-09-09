c8143ec7a5936db3a4c42cbac0e71a3d8e54706f
Angule Mathias <angulemathias3@gmail.com>
2018-09-10 00:18:04 +0300

    [CHORE #160364432]- test POST empty question

--- app/tests/test_post_question.py
+++ app/tests/test_post_question.py
@@ -54,3 +54,25 @@ class TestPostQuestion(BaseTestCase):
         reply = json.loads(response2.data)
         self.assertEqual(reply.get("message"), "Question already exists, check it out for an answer")
         self.assertEqual(response2.status_code, 409)
+
+    def test_post_empty_question(self):
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(dict(title="What", question=" "), )
+                                  )
+
+        reply = json.loads(response2.data)
+        self.assertEqual(reply.get("message"), "No question was given")
+        self.assertEqual(response2.status_code, 400)
