a7b67c7a5c449d45af96cb2ee7bced75d4fd4322
Angule Mathias <angulemathias3@gmail.com>
2018-09-09 21:00:55 +0300

    [CHORE #160364432]- test view questions by id

--- app/tests/test_view_questions.py
+++ app/tests/test_view_questions.py
@@ -35,16 +35,38 @@ class TestViewQuestion(BaseTestCase):
                                  data=json.dumps(dict(username="angule", password="mathias"))
                                  )
         reply2 = json.loads(response.data.decode())
-
         response2 = self.app.post("/api/v1/questions",
                                   content_type='application/json',
-                                  headers=dict(Authorization='Bearer ' + reply2[0]['token']),
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
                                   data=json.dumps(
                                       dict(title="What", question="What is your question?"), )
                                   )
         response3 = self.app.get("/api/v1/questions", content_type='application/json',
-                                 headers=dict(Authorization='Bearer ' + reply2[0]['token']))
+                                 headers=dict(Authorization='Bearer ' + reply2[1]['token']))
         self.assertEqual(response3.status_code, 200)
+
+    def test_viewing_single_question(self):
+        """ Test viewing questions """
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(
+                                      dict(title="Life", question="Are there so many questions about life?"), )
+                                  )
+        response3 = self.app.get("/api/v1/questions/1", content_type='application/json',
+                                 headers=dict(Authorization='Bearer ' + reply2[1]['token']), data={"qstn_id": "1"})
+        self.assertEquals(response3.status_code, 200)
 
     def test_viewing_all_user_questions_(self):
         """ Test viewing questions """
@@ -53,16 +75,14 @@ class TestViewQuestion(BaseTestCase):
                                   data=json.dumps(
                                       dict(username="angule", email="angule@gmail.com", password="mathias"), )
                                   )
-        response = self.app.post(
-            "/api/v1/auth/login",
-            content_type='application/json',
-            data=json.dumps(dict(username="angule", password="mathias"))
-        )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
         reply2 = json.loads(response.data.decode())
-
         response3 = self.app.get("/api/v1/questions/user_questions", content_type='application/json',
                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
                                  data={"qstn_owner": "angule"})
         reply = json.loads(response3.data.decode())
-        self.assertEquals(reply.get("message"), "user has no questions")
-        self.assertEquals(response3.status_code, 404)
+        self.assertEqual(reply.get("message"), "user has no questions")
+        self.assertEqual(response3.status_code, 404)
