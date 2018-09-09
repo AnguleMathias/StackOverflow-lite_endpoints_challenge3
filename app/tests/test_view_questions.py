18f2940f023a46a277e3c1482bc673a9886063c6
Angule Mathias <angulemathias3@gmail.com>
2018-09-09 20:55:55 +0300

    [CHORE #160364432]- test view all questions

--- app/tests/test_view_questions.py
+++ app/tests/test_view_questions.py
@@ -20,8 +20,31 @@ class TestViewQuestion(BaseTestCase):
         response3 = self.app.get("/api/v1/questions", content_type='application/json',
                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']))
         reply = json.loads(response3.data)
-        self.assertEquals(reply.get("message"), "No questions posted yet")
-        self.assertEquals(response3.status_code, 404)
+        self.assertEqual(reply.get("message"), "No questions posted yet")
+        self.assertEqual(response3.status_code, 404)
+
+    def test_viewing_all_questions(self):
+        """ Test viewing questions """
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[0]['token']),
+                                  data=json.dumps(
+                                      dict(title="What", question="What is your question?"), )
+                                  )
+        response3 = self.app.get("/api/v1/questions", content_type='application/json',
+                                 headers=dict(Authorization='Bearer ' + reply2[0]['token']))
+        self.assertEqual(response3.status_code, 200)
 
     def test_viewing_all_user_questions_(self):
         """ Test viewing questions """
