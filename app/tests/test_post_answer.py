d4725def74f86922b4eda46274c726898a61fae5
Angule Mathias <angulemathias3@gmail.com>
2018-09-10 01:01:49 +0300

    [CHORE #160364432]- Initial test update answer

--- app/tests/test_post_answer.py
+++ app/tests/test_post_answer.py
@@ -27,4 +27,48 @@ class TestPostAnswer(BaseTestCase):
                                   headers=dict(Authorization='Bearer ' + reply2[1]['token']),
                                   data=json.dumps(dict(answer="What is your question?"), )
                                   )
-        self.assertEquals(response3.status_code, 201)
+        self.assertEqual(response3.status_code, 201)
+
+    def test_updating_answer_as_answer_owner(self):
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+
+        response1 = self.app.post("/api/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(
+                                      dict(title="What", question="What is your question?"), )
+                                  )
+        _response = self.app.post("/api/v1/auth/login",
+                                  content_type='application/json',
+                                  data=json.dumps(dict(username="angule", password="araali"))
+                                  )
+        reply3 = json.loads(_response.data.decode())
+
+        response3 = self.app.post("/api/questions/1/answers",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply3['token']),
+                                  data=json.dumps(dict(answer="What is your question?"), )
+                                  )
+        response4 = self.app.put("/api/questions/1/answers/1",
+                                 content_type='application/json',
+                                 headers=dict(Authorization='Bearer ' + reply3['token']),
+                                 data=json.dumps(dict(answer="Do you have a question?"), )
+                                 )
+        reply4 = json.loads(response4.data)
+        self.assertEqual(reply4.get("message"), "Answer successfully updated")
+        self.assertEqual(response4.status_code, 200)
