1cb331276cf59ba35d4bb468d906c94c18ff587f
Angule Mathias <angulemathias3@gmail.com>
2018-09-11 07:42:04 +0300

    [CHORE #160364432]- Test View all user questions

--- tests/test_view_questions.py
+++ tests/test_view_questions.py
@@ -167,3 +167,27 @@ class TestViewQuestion(BaseTestCase):
         reply = json.loads(response3.data.decode())
         self.assertEqual(reply.get("message"), "user has no questions")
         self.assertEqual(response3.status_code, 404)
+
+    def test_viewing_all_user_questions(self):
+        """ Test viewing questions """
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
+                                  )
+        response = self.app.post(
+            "/api/v1/auth/login",
+            content_type='application/json',
+            data=json.dumps(dict(username="angule", password="mathias"))
+        )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(
+                                      dict(title="What", question="What is your question?"), )
+                                  )
+        response3 = self.app.get("/api/v1/questions/user_questions", content_type='application/json',
+                                 headers=dict(Authorization='Bearer ' + reply2[1]['token']), data={"qstn_id": "1"})
+        self.assertEquals(response3.status_code, 200)
