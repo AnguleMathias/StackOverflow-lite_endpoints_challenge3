b0251f6456a6dd5502310684ba25504ff0519aee
Angule Mathias <angulemathias3@gmail.com>
2018-09-11 07:04:38 +0300

    [FIX]- Refactoring tests

--- tests/test_post_answer.py
+++ tests/test_post_answer.py
@@ -127,7 +127,7 @@ class TestPostAnswer(BaseTestCase):
         response1 = self.app.post("/api/v1/auth/register",
                                   content_type='application/json',
                                   data=json.dumps(
-                                      dict(username="mathias", email="angule@gmail.com", password="angule"), )
+                                      dict(username="angule", email="angule@gmail.com", password="mathias"), )
                                   )
         response = self.app.post("/api/v1/auth/login",
                                  content_type='application/json',
@@ -143,7 +143,7 @@ class TestPostAnswer(BaseTestCase):
                                   )
         _response = self.app.post("/api/v1/auth/login",
                                   content_type='application/json',
-                                  data=json.dumps(dict(username="mathias", password="angule"))
+                                  data=json.dumps(dict(username="angule", password="mathias"))
                                   )
         reply3 = json.loads(_response.data.decode())
 
@@ -159,7 +159,7 @@ class TestPostAnswer(BaseTestCase):
                                  data=json.dumps(dict(answer="Do you have a question?"), )
                                  )
         reply4 = json.loads(response4.data)
-        self.assertEqual(reply4.get("message"), "No such answer exists")
+        self.assertEqual(reply4.get("message"), "No such question exists any more")
         self.assertEqual(response4.status_code, 404)
 
     def test_update_nonexistant_answer_as_question_owner(self):
