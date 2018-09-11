587a6ccea4e9643183d29cc601a9573943583aa5
Angule Mathias <angulemathias3@gmail.com>
2018-09-11 07:21:25 +0300

    [FIX]- Refactoring test view questions

--- tests/test_view_questions.py
+++ tests/test_view_questions.py
@@ -98,31 +98,31 @@ class TestViewQuestion(BaseTestCase):
         self.assertEqual(reply.get("message"), "Question successfully deleted")
         self.assertEqual(response3.status_code, 200)
 
-    # def test_deleting_a_questions_with_improper_id(self):
-    #     """ Test deleting questions """
-    #     response1 = self.app.post("/api/v1/auth/register",
-    #                               content_type='application/json',
-    #                               data=json.dumps(
-    #                                   dict(username="angule", email="angule@email.com", password="mathias"), )
-    #                               )
-    #     response = self.app.post("/api/v1/auth/login",
-    #                              content_type='application/json',
-    #                              data=json.dumps(dict(username="angule", password="mathias"))
-    #                              )
-    #     reply2 = json.loads(response.data.decode())
-    #
-    #     response2 = self.app.post("/api/v1/questions",
-    #                               content_type='application/json',
-    #                               headers=dict(Authorization='Bearer ' + reply2[1]['token']),
-    #                               data=json.dumps(
-    #                                   dict(title="What", question="What is your question?"), )
-    #                               )
-    #     response3 = self.app.delete("/api/v1/questions/r", content_type='application/json',
-    #                                 headers=dict(Authorization='Bearer ' + reply2[1]['token']))
-    #
-    #     reply = json.loads(response3.data)
-    #     self.assertEqual(reply.get("message"), "Question id should be an integer")
-    #     self.assertEqual(response3.status_code, 400)
+    def test_deleting_a_questions_with_improper_id(self):
+        """ Test deleting questions """
+        response1 = self.app.post("/api/v1/auth/register",
+                                  content_type='application/json',
+                                  data=json.dumps(
+                                      dict(username="angule", email="angule@email.com", password="mathias"), )
+                                  )
+        response = self.app.post("/api/v1/auth/login",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(username="angule", password="mathias"))
+                                 )
+        reply2 = json.loads(response.data.decode())
+
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  headers=dict(Authorization='Bearer ' + reply2[1]['token']),
+                                  data=json.dumps(
+                                      dict(title="What", question="What is your question?"), )
+                                  )
+        response3 = self.app.delete("/api/v1/questions/r", content_type='application/json',
+                                    headers=dict(Authorization='Bearer ' + reply2[1]['token']))
+
+        reply = json.loads(response3.data)
+        self.assertEqual(reply.get("message"), "Id should be an interger")
+        self.assertEqual(response3.status_code, 400)
 
     def test_deleting_non_existing_questions(self):
         response1 = self.app.post("/api/v1/auth/register",
